4a30db4259d9fb214a54dce755f68c50d6263218
Koki Takahashi <hakatasiloving@gmail.com>
2016-01-05 22:12:11 +0900

    Remove test.js

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const chars = require('../');
-
-describe('Basic Usage', () => {
-	it('basically works', () => {
-		assert.deepStrictEqual(chars('foo bar'), ['f', 'o', 'o', ' ', 'b', 'a', 'r']);
-	});
-});
